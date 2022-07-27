6ebc08f2c6cefa5b897fc03fd0833950f2717b13
lieur-euy <yayatkasafa1304@gmail.com>
2022-07-27 11:02:08 +0700

    pdf1

---
+++ MVV/resources/views/pdf/psb_pln_site.blade.php
@@ -0,0 +1,15 @@
+<style>
+    @page {
+        size: 21cm 29.7cm;
+        margin: 30mm 45mm 30mm 45mm;
+    }
+</style>
+
+
+<!DOCTYPE html>
+<head>
+    <header>
+        <img src="pdf/Logo_Mitratel.png" >
+    </header>
+</head>
+</html>
